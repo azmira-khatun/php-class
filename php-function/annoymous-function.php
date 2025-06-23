c1b4488bbaae9f183c072ca974d03b209ffe60d9
azmira-khatun <azmiragrn@gmail.com>
2025-06-23 10:35:24 +0600

    new

---
+++ php-function/annoymous-function.php
@@ -0,0 +1,41 @@
+<?php
+
+// ja function ar kno name nai taka annoymous function bola
+$f=function(){
+    echo "hi";
+};
+$f();
+
+echo "<br>";
+
+// normal function
+// $v=function info(){
+//     echo "hello bamgladesh";
+// };
+// info();
+
+
+
+
+// annoymous function
+
+$add=function($a,$b){
+    return $a+$b;
+};
+echo $add(2,3);
+echo "<br>";
+
+
+// arrow function
+$addArrow=fn($a,$b) =>$a+$b;
+echo $addArrow(2,3);
+
+
+
+echo "<br>";
+
+$info=fn() =>"hello";
+echo $info();
+
+
+?>
