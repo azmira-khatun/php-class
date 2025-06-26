0a59b7bd6829d21c58fcdc93117b4a3b5d42c6f7
azmira-khatun <azmiragrn@gmail.com>
2025-06-26 10:29:19 +0600

    new

---
+++ prime_number.php/index1.php
@@ -0,0 +1,31 @@
+
+
+<?php
+
+function isPrime($num) {
+    if ($num < 2) {
+        return false;
+    }
+
+    for ($i = 2; $i <= sqrt($num); $i++) {
+        if ($num % $i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+for ($i = 1; $i <= 100; $i++) {
+    if (isPrime($i)) {
+        echo $i . " ";
+    }
+}
+
+?>
+
+
+
+
+
+
