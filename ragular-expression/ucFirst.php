5c9e5f49fafce592618c63367c7822fd8d9dbf6a
azmira-khatun <azmiragrn@gmail.com>
2025-07-05 09:17:31 +0600

    new

---
+++ ragular-expression/ucFirst.php
@@ -0,0 +1,10 @@
+
+<?php
+// The ucfirst() function converts the first character of a string to uppercase.
+
+
+$a="new york city is a big city";
+
+	echo ucfirst($a);
+
+?>
---
+++ ragular-expression/ucWords.php
@@ -0,0 +1,10 @@
+
+<?php
+// The ucwords() function converts the first character of each word in a string to uppercase.
+
+
+$a="new york city";
+
+	echo ucwords($a);
+
+?>
