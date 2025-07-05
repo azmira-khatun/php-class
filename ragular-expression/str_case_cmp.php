05d6f83d6490ac02950973ccbb88aa3883d9e880
azmira-khatun <azmiragrn@gmail.com>
2025-07-05 09:13:02 +0600

    new

---
+++ ragular-expression/str_case_cmp.php
@@ -0,0 +1,17 @@
+<?php
+// 	Compares two strings (case-insensitive)
+// strcasecmp(string1,string2)
+
+$c;"<br/>";
+ //1
+ $x = "MY ID IS 1288188";
+ $y = "my id is 1288188";
+
+ $z =strcasecmp($x,$y);
+ echo $z."<br>";
+  $s = "SHARMIN@1288100";
+ $t = "sharmin@1288188";
+
+ $u =strcasecmp($s,$t);
+ echo $u;
+?>
---
+++ ragular-expression/strcspm.php
@@ -0,0 +1,9 @@
+
+<?php
+// The strcspn() function returns the number of characters (including whitespaces) found in a string before any part of the specified characters are found.
+
+$t ="adbhsjh1244";
+$s =strcspn($t,"1244");
+echo $s;
+
+?>
