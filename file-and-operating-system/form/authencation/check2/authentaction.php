2266cfc0d531537f330d33df6a1e322ca2221c99
azmira-khatun <azmiragrn@gmail.com>
2025-07-09 10:44:51 +0600

    new

---
+++ file-and-operating-system/form/authencation/check2/authentaction.php
@@ -0,0 +1,47 @@
+<?php
+if(isset($_POST['btnLogin'])){ 
+    $username=$_POST['txtUsername'];
+    $password=$_POST["txtPassword"];
+    $file=file("data.txt");
+
+    foreach($file as $line){
+        list($_username,$_password)=explode(",",trim( $line));
+        if($_username==$username && $_password==$password){
+            header("location:data.php");
+        }else{
+            $msg="username or password is incorrect";
+        }
+    }
+
+
+
+
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo isset($msg)?$msg:"";
+    ?>
+    <form action="#" method="post"> 
+    <div>
+    Username<br/>
+    <input type="text" name="txtUsername" />
+   </div>
+   <div>
+    Password<br/>
+    <input type="password" name="txtPassword" />
+   </div>
+   <div>
+    <input type="submit" value="Log In" name="btnLogin" />
+   </div>
+    </form>
+</body>
+</html>
