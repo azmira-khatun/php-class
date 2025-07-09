23089641e0c9cefd617a5bfd084d98237f73c639
azmira-khatun <azmiragrn@gmail.com>
2025-07-09 10:58:02 +0600

    new

--- file-and-operating-system/form/authencation/check2/data.php
+++ file-and-operating-system/form/authencation/check2/data.php
@@ -4,8 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        body{
+            background-color: aliceblue;
+            text-align: center;
+            margin: 40px auto;
+            font-weight: bolder;
+        }
+        img{
+            width: 400px;
+            height: 400px;
+        }
+    </style>
+
 </head>
 <body>
     <h1>this is me.</h1>
+    <div>
+        <img src="https://icon-library.com/images/desktop-computer-icon-png/desktop-computer-icon-png-1.jpg" alt="" >
+    </div>
 </body>
 </html>
