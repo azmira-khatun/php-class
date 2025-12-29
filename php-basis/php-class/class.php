bd6eb4e2753d19cad51e24c86ff88ccb3201ffbe
azmira-khatun <azmiragrn@gmail.com>
2025-12-29 12:39:11 +0600

    first commit

---
+++ php-basis/php-class/class.php
@@ -0,0 +1,20 @@
+<?php
+class Car {
+    // Property
+    public $color;
+
+    // Constructor
+    function __construct($color){
+        $this->color = $color;
+    }
+
+    // Method
+    function drive(){
+        echo "The $this->color car is driving";
+    }
+}
+
+// Object creation
+$car1 = new Car("Red");
+$car1->drive(); // Output: The Red car is driving
+?>
